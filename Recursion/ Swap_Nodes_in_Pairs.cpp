c77e6f80b28ba74157552936d05c440258d24790
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-08 19:18:47 +0530

    Update  Swap_Nodes_in_Pairs.cpp

--- Recursion/ Swap_Nodes_in_Pairs.cpp
+++ Recursion/ Swap_Nodes_in_Pairs.cpp
@@ -1,15 +1,21 @@
+//Question:- (https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681/)
+
 class Solution 
 {
 public:
     ListNode* swapPairs(ListNode* head) 
     {
+        //If there is 0 or 1 element, swapping will not occur
         if(!head || !head->next)
             return head;
             
         ListNode* nextNode = head->next;
+        //First swap rest of the pairs
         head->next = swapPairs(nextNode->next);
+        //swap current pair
         nextNode->next = head;
         
+        //head is swapped by nexttNode
         return nextNode;
     }
 };
