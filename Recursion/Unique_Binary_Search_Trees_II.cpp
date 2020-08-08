8769fce409961c54ba4b1217704d14636c867553
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-08 19:14:19 +0530

    Update Unique_Binary_Search_Trees_II.cpp

--- Recursion/Unique_Binary_Search_Trees_II.cpp
+++ Recursion/Unique_Binary_Search_Trees_II.cpp
@@ -1,3 +1,9 @@
+// Question:- (https://leetcode.com/explore/featured/card/recursion-i/253/conclusion/2384/)
+/*
+I start by noting that 1..n is the in-order traversal for any BST with nodes 1 to n. So if I pick i-th node as my root,
+the left subtree will contain elements 1 to (i-1), and the right subtree will contain elements (i+1) to n. 
+I use recursive calls to get back all possible trees for left and right subtrees and combine them in all possible ways with the root.
+*/
 class Solution
 {
 public:
@@ -28,6 +34,7 @@ public:
     }
     vector<TreeNode*> generateTrees(int n)
     {
+        //if n is 0 then no tree can be genegrated
         if(n == 0)
             return {};
         
