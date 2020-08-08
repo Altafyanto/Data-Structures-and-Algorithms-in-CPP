f21f604a1764959cd07bbbe8c7d26923c4b92c41
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-08 19:07:45 +0530

    Update Reverse_Linked_List.cpp

--- Recursion/Reverse_Linked_List.cpp
+++ Recursion/Reverse_Linked_List.cpp
@@ -1,3 +1,9 @@
+//Question:- (https://leetcode.com/explore/featured/card/recursion-i/251/scenario-i-recurrence-relation/2378/)
+/*
+    Input: 1->2->3->4->5->NULL
+    Output: 5->4->3->2->1->NULL
+*/
+
 class Solution
 {
 public:
