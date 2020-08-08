e7d2dcd9e1c067eb53300648d42848be742088f6
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-08 18:56:51 +0530

    Update Merge_Two_Sorted_Lists.cpp

--- Recursion/Merge_Two_Sorted_Lists.cpp
+++ Recursion/Merge_Two_Sorted_Lists.cpp
@@ -1,16 +1,21 @@
+[Question link](https://leetcode.com/explore/featured/card/recursion-i/253/conclusion/2382/)
+                
 class Solution
 {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
     {
+        //If first is null, then return entire second list
         if(!l1)
             return l2;
-
+        
+        //If second is null, then return entire first list
         if(!l2)
             return l1;
 
         ListNode* node = new ListNode();
-
+        
+        //add element which is smaller one 
         if(l1->val < l2->val)
         {
             node->val = l1->val;
