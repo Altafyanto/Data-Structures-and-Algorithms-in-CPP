e95f3bfdab1ccbe530ef95cee1315fab5c21d595
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-10 15:20:06 +0530

    Create Tower_Of_Hanoi.cpp

---
+++ Recursion/Tower_Of_Hanoi.cpp
@@ -0,0 +1,35 @@
+  
+/*
+Question:- We are given three towers one is source , one is helper other is destination . We have to shift all the disks from source to destnation using helper tower.
+There are some conditions which are:-
+1. We can't put larger disk on smaller one.
+2. We can take only one disk at a time.
+3. We can take help of helper tower to shift disks.
+link :- (https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/)
+*/
+
+/*
+Approach:-
+1. If there is only one disk then simply shift it at destination from source.
+2. Otherwise, Put all the disks at helper tower except the last one and move last disk to destonation.
+3. Atlast move remaining n-1 disks from helper to destination.
+*/
+
+int towerOfHanoi(int n,int source,int destination,int helper)
+{
+    //If only one disk then move disk and return
+    if(n == 1)
+    {
+        cout << "move disk 1 From " << source << " To " << destination << "\n";
+        return;
+    }
+    
+    //Move Top n-1 disks from source to helper
+    towerOfHanoi(n-1,source,helper,destination);
+    
+    //Move last disk From Source to Destination
+    cout << "move disk " << n << " From " << source << " To " << destination << "\n";
+    
+    //Move n-1 disks from helper to destination
+    towerOfHanoi(n-1,helper,destination,source);
+}
