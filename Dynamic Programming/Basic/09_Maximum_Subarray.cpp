7a4705678975abb60f19ba0cd4b10c8dbe048823
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-09-10 20:24:37 +0530

    Create 09_Maximum_Subarray.cpp

---
+++ Dynamic Programming/Basic/09_Maximum_Subarray.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+//Time Complexity - O(N), Space Complexity -O(1)
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+
+        // We have to take atleast one element
+        int result = nums[0], previous = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            // Adding previpous result iff it is positive
+            previous = nums[i] + (previous > 0 ? previous : 0);
+            result = max(result, previous);
+        }
+
+        return result;
+    }
+};
