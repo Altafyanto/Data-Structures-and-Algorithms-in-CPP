37514c60bfbf663deccc7fa248f212547c941ddf
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-09-10 19:19:49 +0530

    Update 12_Count_number_of_hops.cpp

--- Dynamic Programming/Top 20 Interview Question/12_Count_number_of_hops.cpp
+++ Dynamic Programming/Top 20 Interview Question/12_Count_number_of_hops.cpp
@@ -6,10 +6,13 @@ using namespace std;
 int countNumberOFHops(int n)
 {
     int DP[n+1];
-    DP[0] = 1, DP[1] = 1, DP[2] = 2;
+    
+    DP[0] = 1; // if no of steps is 0 then there frog cannot jump so this is the only way.
+    DP[1] = 1; // if no of steps is 1 then frog can jump only once.
+    DP[2] = 2; // if no of steps is 2 then frog can either jump 1 step 2 times or 2 steps long jump once ,then it will be two ways.
 
     for (int i = 3; i <= n; i++)
-        DP[i] = DP[i-1] + DP[i-2] + DP[i-3];
+        DP[i] = DP[i-1] + DP[i-2] + DP[i-3]; // in all other cases total no of ways to reach top will be the sum of ways for 1 step , 2 steps and 3 steps long jump.
 
     return DP[n];
 }
