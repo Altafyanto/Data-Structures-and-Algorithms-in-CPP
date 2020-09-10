9bb11a8e5dea1a19f8adacff2af888a432360e94
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-09-10 22:13:00 +0530

    Update 16_Optimal_Strategy_For_A_Game.cpp

--- Dynamic Programming/Top 20 Interview Question/16_Optimal_Strategy_For_A_Game.cpp
+++ Dynamic Programming/Top 20 Interview Question/16_Optimal_Strategy_For_A_Game.cpp
@@ -8,12 +8,15 @@ int DP[101][101];
 
 int optimalStrategy(int i, int j, int sum)
 {
+    // if opponent chooses choice next to user's choice then he will leave the lesser for user and from that user will choose maximum one.
     if (j == i + 1)
         return max(arr[i], arr[j]);
 
+    // if points are already processed for some values of choice of coins then we will just use the pre-processed ans.
     if (DP[i][j] != -1)
         return DP[i][j];
 
+    // to get the maximum coins , the user will choose from the coins which will remain after making opponent choose the minimum coin.
     return DP[i][j] = sum - min(optimalStrategy(i, j - 1, sum - arr[j]), optimalStrategy(i + 1, j, sum - arr[i]));
 }
 
