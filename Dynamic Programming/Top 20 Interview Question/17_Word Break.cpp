125d02d8f733d98f8f7af539a614697f3543a460
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-09-11 15:10:25 +0530

    Update 17_Word Break.cpp

--- Dynamic Programming/Top 20 Interview Question/17_Word Break.cpp
+++ Dynamic Programming/Top 20 Interview Question/17_Word Break.cpp
@@ -8,27 +8,34 @@ using namespace std;
 int wordBreak(unordered_set<string> &dict, string word)
 {
     int size = word.size();
+    
+    // empty string will always be present in dictionary.
     if (size == 0)
         return 1;
-
+    
     int DP[size + 1];
     memset(DP, 0, sizeof(DP)); //Initialize with false
 
     for (int i = 1; i <= size; i++)
     {
+        // if any letter is not traversed yet and sustring upto that letter is present in dictionary then return 1.
         if (DP[i] == 0 && dict.find(word.substr(0, i)) != dict.end())
             DP[i] = 1;
 
+        // if a letter is traversed before and also substing upto it is present in dictionary
         if (DP[i] == 1)
         {
+            // if we reach at the last letter than it will be present in dictionary.
             if (i == size)
                 return 1;
 
+            // otherwise we will traverse from i+1 letter to size of the string and will check for the substring formed by remianing letters.
             for (int j = i + 1; j <= size; j++)
             {
                 if (DP[j] == 0 && dict.find(word.substr(i, j - i)) != dict.end())
                     DP[j] = 1;
 
+                // if we are at last letter, if substring including all letters upto last letter is present in dictionary then will return 1.
                 if (j == size && DP[size] == 1)
                     return 1;
             }
