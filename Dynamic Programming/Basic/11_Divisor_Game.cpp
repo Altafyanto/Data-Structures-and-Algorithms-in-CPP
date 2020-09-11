13b342a1af080a7b10b260415706a6ee866fbeb7
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-09-11 19:48:52 +0530

    Update 11_Divisor_Game.cpp
    check other solutions in discussion section and no need to check upto i/2 . you should check upto root i. Because we check upto i/2 if we have add find sum but here he have to check factor.

--- Dynamic Programming/Basic/11_Divisor_Game.cpp
+++ Dynamic Programming/Basic/11_Divisor_Game.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/divisor-game/
 
+//Solution 1: Using DP
 class Solution {
 public:
     bool divisorGame(int N) {
@@ -13,10 +14,10 @@ public:
         {
             bool temp = false;
             
-            // We only need to check upto i/2 because if bob lose upto i/2 then he will loose the game.
-            for(int j=1;j<=i/2;j++)
+            // We only need to check upto root of i because if bob lose upto i/2 then he will loose the game.
+            for(int j=1;j*j<=i;j++)
             {
-                // if after choosing j upto i/2 it is divisible by j and and after replacing the value with i-j DP is giving false 
+                // if after choosing j upto root of i it is divisible by j and and after replacing the value with i-j DP is giving false 
                 // means Bob is not able to make a move so in that case alice will win.
                 if(i%j==0)
                 {
@@ -30,5 +31,15 @@ public:
             dp[i] = temp;
         }
         return dp[N];
+    }
+};
+
+// solution 2:
+class Solution 
+{
+public:
+    bool divisorGame(int N)
+    {
+        return (!(N&1));//Alice will win iff n is even number
     }
 };
