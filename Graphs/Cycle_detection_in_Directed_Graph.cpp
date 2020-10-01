41332c2b4fedf751ebf00a1f040c9274c9a6d998
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-10-01 15:37:17 +0530

    Update Cycle_detection_in_Directed_Graph.cpp

--- Graphs/Cycle_detection_in_Directed_Graph.cpp
+++ Graphs/Cycle_detection_in_Directed_Graph.cpp
@@ -1,14 +1,21 @@
+/* Time-complexity:- O(V+E) where V is the no of vertices and E is the no of edges.
+   Space-complexity:- O(V) where V is the no of vertices.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 bool isCyclicUtil(vector<int> adj[], vector<bool> vis, int curr)
 {
+    // when we have visited a vertice which has been already visited before then we detected a cycle.
     if (vis[curr] == true)
         return true;
-
+    
+    // otherwise just make it visited.
     vis[curr] = true;
     bool flag = false;
-
+    
+    // traversing the adjacency list of current vertice and checking if there any cycle present or not if yes then make flag true otherwise false.
     for (int i = 0; i < adj[curr].size(); i++)
     {
         flag = isCyclicUtil(adj, vis, adj[curr][i]);
@@ -20,8 +27,10 @@ bool isCyclicUtil(vector<int> adj[], vector<bool> vis, int curr)
 
 bool isCyclic(int vertices, vector<int> adj[])
 {
+    // making a boolean array named visited to mark true if cycle is present otherwise false.
     vector<bool> vis(vertices, false);
     bool flag = false;
+    // for each loop for to check cycle in adjacency lsit of every vertice.
     for (int i = 0; i < vertices; i++)
     {
         vis[i] = true;
